f1d7ed3d16e83e7827db857a5bd047735a338f00
beshoynasryz <beshoy.nasry@tradesocio.com>
2025-06-18 23:33:05 +0300

    use Hash on Create user function to hash pass

--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -1,36 +1,37 @@
-import User from "./user.model";
-import expressAsyncHandler from "express-async-handler";
-import bcrypt from "bcrypt";
-
-
+import User from './user.model';
+import expressAsyncHandler from 'express-async-handler';
+import bcrypt from 'bcrypt';
+import { Hash } from '../../utils/Hash/index.js';
 
 export const createUser = expressAsyncHandler(async (req, res, next) => {
+	const { name, email, password, phone, role } = req.body;
 
-    const { name, email, password, phone, role } = req.body;
+	if (!name || !email || !password || !phone) {
+		return next(new Error('All fields are required'));
+	}
+	const existingUser = User.findOne({ email });
+	if (existingUser) {
+		return res.status(400).send({ message: 'User is already exists' });
+	}
+	const hashedPassword = await Hash({
+		key: password,
+		SALT_ROUNDS: process.env.SALT_ROUNDS
+	});
 
-    if (!name || !email || !password || !phone) {
-            return next(new Error("All fields are required"));   
-        }
-        const existingUser = User.findOne({email})
-        if( existingUser ){
-            return res.status(400).send({message:"User is already exists"})
-        }
-        const hashedPassword = bcrypt.hash(10, password)
-        
-        const user = await User.create({ name, email, password:hashedPassword, phone, role });
-        const {password: _,...userWithoutPassowrd } = user.toObject()
-   
+	const user = await User.create({
+		name,
+		email,
+		password: hashedPassword,
+		phone,
+		role
+	});
+	const { password: _, ...userWithoutPassowrd } = user.toObject();
 
-        res.status(201).json({
-            success: true,
-            message: "User created successfully",
-            data: userWithoutPassowrd
-        });
+	res.status(201).json({
+		success: true,
+		message: 'User created successfully',
+		data: userWithoutPassowrd
+	});
+});
 
-
-})
-
-export const getUserById = expressAsyncHandler(async (req, res, next) => {
-     
-
-})
+export const getUserById = expressAsyncHandler(async (req, res, next) => {});
